a1debe5d5c829db4aab2ac716e051c3cca8cce08
zmisgod <starzmisgod@gmail.com>
2017-12-26 23:27:08 +0800

    new

---
+++ README.md
@@ -0,0 +1,20 @@
+# goSpider
+
+learn to write a spider with golang
+
+target url  : http://www.alfuli.com/fuliba
+
+expact: save images
+
+## day one
+
+- [x] get url and page title
+- [ ] create a folder name is page title
+- [ ] download images
+- [ ] optimize code
+
+## contact
+
+<a href="https://zmis.me">zmis.me新博客</a>
+
+<a href="https://weibo.com/zmisgod">@zmisgod</a>
---
+++ entry.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+var url = "http://www.alfuli.com/fuliba"
+var responseBody string
+
+func main() {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	responseBody = string(body)
+	// println(string(responseBody))
+	var re = regexp.MustCompile(`<article .*>(.*?)<\/article>`)
+	if len(re.FindString(responseBody)) > 0 {
+		var responseBody = re.FindString(responseBody)
+		var rest = regexp.MustCompile(`[<]a\starget=\"_blank\"\shref=\"([a-zA-z]+://[^\s]*)\"\stitle=\"(.*?)\"[>]{1}(.*?)</a>`)
+		var result = rest.FindAllString(responseBody, -1)
+		for _, v := range result {
+			//url
+			var urlRegexp = regexp.MustCompile(`([a-zA-z]+:[^\s]*)`)
+			var url = strings.Replace(urlRegexp.FindString(v), "\"", "", -1)
+			//file name
+			fileNameRegexp, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+			src := strings.Replace(fileNameRegexp.ReplaceAllString(v, "\n"), " ", "", -1)
+			println(src)
+			println(url)
+		}
+	}
+}
