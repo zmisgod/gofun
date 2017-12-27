a394b6e8ee6236470c1bfe1383cbbcb84c8ed2e7
zmisgod <starzmisgod@gmail.com>
2017-12-27 23:25:07 +0800

    new

---
+++ spider.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	responseBody = string(body)
+	var re = regexp.MustCompile(`<article .*>(.*?)<\/article>`)
+	if len(re.FindString(responseBody)) > 0 {
+		var responseBody = re.FindString(responseBody)
+		var rest = regexp.MustCompile(`[<]a\starget=\"_blank\"\shref=\"([a-zA-z]+://[^\s]*)\"\stitle=\"(.*?)\"[>]{1}(.*?)</a>`)
+		var result = rest.FindAllString(responseBody, -1)
+		for _, v := range result {
+			//url
+			var urlRegexp = regexp.MustCompile(`([a-zA-z]+:[^\s]*)`)
+			var url = strings.Replace(urlRegexp.FindString(v), "\"", "", -1)
+			//file name
+			fileNameRegexp, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+			src := strings.Replace(fileNameRegexp.ReplaceAllString(v, "\n"), " ", "", -1)
+			println(src)
+			println(url)
+		}
+	}
+}
