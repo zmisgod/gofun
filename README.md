a53b8c9c654f8c76658d3cb4b66ebfb2aa03ce74
zmisgod <starzmisgod@gmail.com>
2018-01-17 22:30:25 +0800

    New

--- README.md
+++ README.md
@@ -8,14 +8,6 @@ expact: save images
 
 status: finish ✅
 
-<hr />
-
-target url:https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=40
-
-expact: save these data into database
-
-status: start
-
 ## useage
 
 ```
@@ -26,6 +18,41 @@ cd /your/path/goSpider
 
 ./goSpider [targetURL] [total pages]
 just wait and do not turn your terminal off
+
+<hr />
+
+target url:https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=40
+
+expact: save these data into database
+
+status: finish ✅
+
+## useage
+
+create a mysql table
+```
+CREATE TABLE `douban_movie` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `rate` varchar(20) DEFAULT NULL COMMENT '评分',
+  `cover` varchar(200) DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL COMMENT '名称',
+  `url` varchar(255) DEFAULT NULL COMMENT '豆瓣地址',
+  `playable` tinyint(1) unsigned DEFAULT NULL COMMENT '是否可播放',
+  `cover_x` varchar(20) DEFAULT NULL COMMENT '?',
+  `cover_y` varchar(20) DEFAULT NULL COMMENT '?',
+  `is_new` tinyint(1) unsigned DEFAULT NULL COMMENT '?',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27658397 DEFAULT CHARSET=utf8;
+```
+then find `doubanSpider.go` replace database config to yours
+
+```
+var dbUser = "root"
+var dbPass = "111111"
+var dbTable = "mytest"
+```
+
+then run ```go run doubanSpider.go```
 
 ## screenshot
 
--- doubanSpider.go
+++ doubanSpider.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var dbUser = "root"
+var dbPass = "111111"
+var dbTable = "mytest"
 var targetURL = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start="
 
+type DoubanData struct {
+	Subjects []detailData `json:"subjects"`
+}
+type detailData struct {
+	Rate     string `json:"rate"`
+	Cover_x  int    `json:"cover_x"`
+	Title    string `json:"title"`
+	Url      string `json:"url"`
+	Playable bool   `json:"playable"`
+	Cover    string `json:"cover"`
+	Id       string `json:"id"`
+	Cover_y  int    `json:"cover_y"`
+	Is_new   bool   `json:"is_new"`
+}
+
 func main() {
-	end := false
+	var isEnd bool
+	startPage := 20
 	for {
-		if end {
+		resEnd := make(chan bool)
+		go fetchURLData(targetURL+strconv.Itoa(startPage), resEnd)
+		isEnd = <-resEnd
+		if isEnd == false {
 			break
+		} else {
+			startPage += 20
 		}
 	}
 }
@@ -28,6 +57,26 @@ func fetchURLData(url string, end chan bool) {
 	if err != nil {
 		end <- true
 	}
-	fmt.Println(respBody)
-	end <- false
+	var data DoubanData
+	err = json.Unmarshal(respBody, &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	db, err := sql.Open("mysql", dbUser+":"+dbPass+"@/"+dbTable+"?charset=utf8")
+	checkErr(err)
+	defer db.Close()
+	for _, v := range data.Subjects {
+		stmt, err := db.Prepare("INSERT ignore INTO douban_movie (id, rate, cover, title, url, playable,cover_x, cover_y, is_new) values (?, ?, ?, ?, ?, ?, ?, ?, ?)")
+		checkErr(err)
+
+		_, err = stmt.Exec(v.Id, v.Rate, v.Cover, v.Title, v.Url, v.Playable, v.Cover_x, v.Cover_y, v.Is_new)
+		checkErr(err)
+	}
+	end <- true
+}
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
